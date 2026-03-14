f6dbb91dd548f907fe1f67dd7d7563310524346f
Kazuki Mizumaru <0922.ka@gmail.com>
2026-03-14 16:00:55 +0900

    add detail pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        houmon: resolve(__dirname, 'houmon/index.html'),
+        jihi: resolve(__dirname, 'jihi/index.html'),
+        isou: resolve(__dirname, 'isou/index.html'),
+        seikatsu: resolve(__dirname, 'seikatsu/index.html'),
+      },
+    },
   },
 })
